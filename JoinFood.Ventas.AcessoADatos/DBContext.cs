1b6400450a0c038947c753ec4c3505d7c96d7bbd
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-20 19:00:40 -0600

    Creación de la clase DbContext

---
+++ JoinFood.Ventas.AcessoADatos/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using JoinFood.Ventas.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+
+namespace JoinFood.Ventas.AcessoADatos
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Categoria>? Categoria { get; set; }
+        public DbSet<Cliente>? Cliente { get; set; }
+        public DbSet<DetalleVenta>? DetalleVenta { get; set; }
+        public DbSet<Producto>? Producto { get; set; }
+        public DbSet<Promocion>? Promocion { get; set; }
+        public DbSet<Rol>? Rol { get; set; }
+        public DbSet<Usuario>? Usuario { get; set; }
+        public DbSet<Venta>? Venta { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=VINNY;Initial Catalog=JoinFood;Integrated Security=True");
+        }
+
+    }
+}
