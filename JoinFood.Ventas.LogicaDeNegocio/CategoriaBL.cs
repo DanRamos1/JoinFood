f443f2826697496d9b9870784a6b5ba4c54da2a4
JMauricio-O <josemauricioortizrivera@gmail.com>
2022-09-20 21:39:03 -0600

    Creacion de la clase CategoriaBL de la capa Logica De Negocio del proyecto

---
+++ JoinFood.Ventas.LogicaDeNegocio/CategoriaBL.cs
@@ -0,0 +1,38 @@
+﻿using JoinFood.Ventas.AcessoADatos;
+using JoinFood.Ventas.EntidadesDeNegocio;
+
+namespace JoinFood.Ventas.LogicaDeNegocio
+{
+    public class CategoriaBL
+    {
+        public async Task<int> Crear(Categoria pCategoria)
+        {
+            return await CategoriaDAL.Crear(pCategoria);
+        }
+
+        public async Task<int> Modificar(Categoria pCategoria)
+        {
+            return await CategoriaDAL.Modificar(pCategoria);
+        }
+
+        public async Task<int> Eliminar(Categoria pCategoria)
+        {
+            return await CategoriaDAL.Eliminar(pCategoria);
+        }
+
+        public async Task<Categoria> ObtenerPorId(Categoria pCategoria)
+        {
+            return await CategoriaDAL.ObtenerPorId(pCategoria);
+        }
+
+        public async Task<List<Categoria>> ObtenerTodos()
+        {
+            return await CategoriaDAL.ObtenerTodos();
+        }
+
+        public async Task<List<Categoria>> Buscar(Categoria pCategoria)
+        {
+            return await CategoriaDAL.Buscar(pCategoria);
+        }
+    }
+}
